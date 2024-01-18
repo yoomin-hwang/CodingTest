df23272c465f83546f458c3b52d351a52c615c0e
Yoomin Hwang <134265429+yoomin-hwang@users.noreply.github.com>
2024-01-18 20:03:49 +0900

    [level 2] Title: 스킬트리, Time: 16.27 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -2,6 +2,19 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(String skill, String[] skill_trees) {
+//         int answer = 0;
+//         ArrayList<String> skillTrees = new ArrayList<String>(Arrays.asList(skill_trees));
+//         //ArrayList<String> skillTrees = new ArrayList<String>();
+//         Iterator<String> it = skillTrees.iterator();
+
+//         while (it.hasNext()) {
+//             if (skill.indexOf(it.next().replaceAll("[^" + skill + "]", "")) != 0) {
+//                 it.remove();
+//             }
+//         }
+//         answer = skillTrees.size();
+//         return answer;
+        
         int answer = 0;
         
         for(String tree : skill_trees){
