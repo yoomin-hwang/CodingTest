fd45482ffd283739596be48c73996c83cb202261
Yoomin Hwang <134265429+yoomin-hwang@users.noreply.github.com>
2024-01-18 19:28:02 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,5 +1,16 @@
 class Solution {
     public boolean solution(String s) {
+        // boolean answer = true;
+        // if(s.length() == 4 || s.length() == 6){
+        //   try{
+        //       int x = Integer.parseInt(s);
+        //       return true;
+        //   } catch(NumberFormatException e){
+        //       return false;
+        //   }
+        // }
+        // else return false;
+        
         boolean answer = true;
         if(s.length() != 4 && s.length() != 6) answer = false;
         for(int i=0; i<s.length(); i++){
